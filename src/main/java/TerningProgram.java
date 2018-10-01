1472319b664879becdcf2163cb0887bfa5129a93
Rasmus <rsoeborg@gmail.com>
2018-10-01 14:54:41 +0200

    Har ændret Dice class og TerningProgram til at være det samme som de filer vi fik fra zip filen.

--- src/main/java/TerningProgram.java
+++ src/main/java/TerningProgram.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class TerningProgram {
     public static void main(String[] args) {
-        Dice d = new Dice();
-
-
+        Dice cup = new Dice();   // Make an instance of Dice
+        Scanner keyboard=new Scanner(System.in);   // Make an instance of keyboard Scanner
+        System.out.println("Welcome to the diceroll program");
+        System.out.println("How many times do you want to roll the dice?");
+        int n = keyboard.nextInt();  // Read integer
+        cup.rollMultiple(n);         // Roll the dice n times
+        keyboard.close();            // Close the scanner
     }
 }
